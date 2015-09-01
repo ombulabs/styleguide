6131162750598fbb85478430c24fcb90b057e5ff
cecilia <cecilia@ombulabs.com>
2015-09-01 16:40:52 -0300

    Cloned from ombushop

---
+++ Rakefile
@@ -0,0 +1,4 @@
+task :sass do
+	`sass stylesheets/main.scss compiled/main.css`
+	puts 'Parsed & compiled main.scss'
+end
---
+++ javascript/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    /**
+     * This part causes smooth scrolling using scrollto.js
+     * We target all a tags inside the nav, and apply the scrollto.js to it.
+     */
+    $("#main-nav a").click(function(evn){
+        evn.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash);
+    });
+
+    /**
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and
+     * manipulation, class adding and class removing, and conditional testing
+     */
+    var aChildren = $("#main-nav li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("nav-active");
+            } else {
+                $("a[href='" + theID + "']").removeClass("nav-active");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("#main-nav li:last-child a").hasClass("nav-active")) {
+                var navActiveCurrent = $(".nav-active").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                $("#main-nav li:first-child a").addClass("nav-active");
+            }
+        }
+    });
+
+    $( "a" ).click(function( event ) {
+      event.preventDefault();
+    });
+
+    $("#nav-button").click(function(){
+      $('#main-nav, #nav-button').toggleClass('open');
+      $('body').toggleClass('modal-active');
+    });
+
+    $('#main-nav li a').click(function(){
+      $('#main-nav, #nav-button').removeClass('open');
+      $('body').removeClass('modal-active');
+    })
+});
