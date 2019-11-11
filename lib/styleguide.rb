6457e3220581ad3a468f3ff1b5abec0ccb93061b
sasha-h-reenhanced <sasha.h+1@reenhanced.com>
2019-11-11 10:39:05 -0500

    Include dependency gems into assets

--- lib/styleguide.rb
+++ lib/styleguide.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'jquery-rails'
+require 'bootstrap-sass'
+require 'popper_js'
+require 'material_design_lite-sass'
+
 module Styleguide
   module Rails
     class Engine < ::Rails::Engine
