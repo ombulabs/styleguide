2537b881f4819d16e47cc45b837d9c48ee62d45c
Ariel Juodziukynas <arieljuod@gmail.com>
2020-10-09 12:51:10 -0300

    Import only the needed modules from material

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("jquery")
 import "bootstrap-sass"
-import "material-design-lite"
+import "material-design-lite/src/mdlComponentHandler"
+import "material-design-lite/src/textfield/textfield"
+import "material-design-lite/src/slider/slider"
 import "./vendor/assets/javascript/fastruby/custom.js"
